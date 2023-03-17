53abe00b63e186131b5a32e0d4d64e41afecd614
devwckd <dev.wckd@gmail.com>
2023-03-17 03:19:31 -0300

    feat: add encryption request packet struct

--- crates/protocol-packets/src/packets/clientbound/login.rs
+++ crates/protocol-packets/src/packets/clientbound/login.rs
@@ -8,3 +8,11 @@ use crate::models::chat::Chat;
 pub struct Disconnect {
     pub reason: Json<Chat>,
 }
+
+#[derive(Packet)]
+#[packet_id = 0x01]
+pub struct EncryptionRequest {
+    pub server_id: String,
+    pub public_key: Vec<u8>,
+    pub verify_token: Vec<u8>,
+}
