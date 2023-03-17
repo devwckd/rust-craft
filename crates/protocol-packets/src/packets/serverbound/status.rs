2c3879b9bb8c5886227dbca416fd66faf731282d
devwckd <dev.wckd@gmail.com>
2023-03-16 22:29:26 -0300

    feat: add ping request and response packets

--- crates/protocol-packets/src/packets/serverbound/status.rs
+++ crates/protocol-packets/src/packets/serverbound/status.rs
@@ -3,3 +3,9 @@ use protocol_derive::Packet;
 #[derive(Packet)]
 #[packet_id = 0x00]
 pub struct StatusRequest {}
+
+#[derive(Packet)]
+#[packet_id = 0x01]
+pub struct PingRequest {
+    pub payload: i64,
+}
