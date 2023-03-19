ba74e5316715b635a80ddf9ff1e2eb38b07cbc1d
João Victor <65056371+devwckd@users.noreply.github.com>
2023-03-19 01:18:54 -0300

    feat: add encryption response packet (#34)

--- crates/protocol-packets/src/packets/serverbound/login.rs
+++ crates/protocol-packets/src/packets/serverbound/login.rs
@@ -7,3 +7,10 @@ pub struct LoginStart {
     pub name: String,
     pub uuid: Option<Uuid>,
 }
+
+#[derive(Packet, Readable, Writeable)]
+#[packet_id = 0x01]
+pub struct EncryptionResponse {
+    pub shared_secret: Vec<u8>,
+    pub verify_token: Vec<u8>,
+}
