6481006b6d9e8d4e2f75ab2a2457908a31162564
João Victor <65056371+devwckd@users.noreply.github.com>
2023-03-22 03:18:41 -0300

    feat: add spawn experience orb packet (#196)

--- crates/protocol-packets/src/packets/clientbound/play.rs
+++ crates/protocol-packets/src/packets/clientbound/play.rs
@@ -19,3 +19,13 @@ pub struct SpawnEntity {
     pub velocity_y: i16,
     pub velocity_z: i16,
 }
+
+#[derive(Packet, Readable, Writeable)]
+#[packet_id = 0x01]
+pub struct SpawnExperienceOrb {
+    pub entity_id: VarInt,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+    pub count: i16,
+}
