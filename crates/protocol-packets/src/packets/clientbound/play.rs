676f03d7650c65088d848f0b438542e8b850ab2d
devwckd <dev.wckd@gmail.com>
2023-03-22 03:01:42 -0300

    feat: add spawn entity packet

---
+++ crates/protocol-packets/src/packets/clientbound/play.rs
@@ -0,0 +1,21 @@
+use protocol_core::data::VarInt;
+use protocol_derive::{Packet, Readable, Writeable};
+use uuid::Uuid;
+
+#[derive(Packet, Readable, Writeable)]
+#[packet_id = 0x00]
+pub struct SpawnEntity {
+    pub entity_id: VarInt,
+    pub entity_uuid: Uuid,
+    pub r#type: VarInt,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+    pub pitch: u8,
+    pub yaw: u8,
+    pub head_yaw: u8,
+    pub data: VarInt,
+    pub velocity_x: i16,
+    pub velocity_y: i16,
+    pub velocity_z: i16,
+}
