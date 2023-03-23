5b8c92de173041a91685ed87c64b1839cc461539
João Victor <65056371+devwckd@users.noreply.github.com>
2023-03-23 06:56:14 -0300

    feat: add rw derive to enums (#205)

--- crates/protocol-derive/src/features/impl_writeable_sync.rs
+++ crates/protocol-derive/src/features/impl_writeable_sync.rs
@@ -1,39 +1,102 @@
 use quote::quote;
 
 pub fn impl_writeable_sync(derive_input: &syn::DeriveInput) -> proc_macro2::TokenStream {
-    if let syn::Data::Struct(s) = &derive_input.data {
-        let mut result_token_stream = proc_macro2::TokenStream::new();
-        for field in &s.fields {
-            match &field.ty {
-                syn::Type::Path(_) => {
-                    let ident = &field.ident;
-                    result_token_stream.extend_one(quote! {
-                        self.#ident.write_sync(write)?;
-                    });
-                }
-                _ => {
-                    panic!()
-                }
-            };
+    return match &derive_input.data {
+        syn::Data::Struct(s) => impl_writeable_sync_struct(s, derive_input),
+        syn::Data::Enum(e) => impl_writeable_sync_enum(e, derive_input),
+        _ => proc_macro2::TokenStream::new(),
+    };
+}
+
+fn impl_writeable_sync_struct(
+    s: &syn::DataStruct,
+    derive_input: &syn::DeriveInput,
+) -> proc_macro2::TokenStream {
+    let result_token_stream = write_inputs(&s.fields, true);
+    let ident = &derive_input.ident;
+
+    let tokens = quote! {
+        impl protocol_core::rw::SyncWriteable for #ident {
+            fn write_sync<T>(&self, mut write: &mut T) -> anyhow::Result<()>
+            where
+                T: std::io::Write
+            {
+                use protocol_core::rw::SyncWriteable;
+
+                #result_token_stream
+
+                Ok(())
+            }
         }
+    };
+    return tokens.into();
+}
 
-        let ident = &derive_input.ident;
+fn impl_writeable_sync_enum(
+    e: &syn::DataEnum,
+    derive_input: &syn::DeriveInput,
+) -> proc_macro2::TokenStream {
+    let ident = &derive_input.ident;
+    let mut match_body = proc_macro2::TokenStream::new();
 
-        let tokens = quote! {
-            impl protocol_core::rw::SyncWriteable for #ident {
-                fn write_sync<T>(&self, write: &mut T) -> anyhow::Result<()>
-                where
-                    T: std::io::Write
-                {
-                    use protocol_core::rw::SyncWriteable;
+    for (index, variant) in e.variants.iter().enumerate() {
+        let index = index as i32;
+        let variant_ident = &variant.ident;
 
-                    #result_token_stream
+        let mut field_idents = proc_macro2::TokenStream::new();
+        for field in &variant.fields {
+            let field_ident = &field.ident;
+            field_idents.extend_one(quote::quote! { #field_ident, });
+        }
+        let result_token_stream = write_inputs(&variant.fields, false);
+        match_body.extend_one(quote::quote! {
+            Self::#variant_ident { #field_idents } => {
+                VarInt::new(#index).write_sync(&mut write)?;
+                #result_token_stream
+            }
+        });
+    }
 
-                    Ok(())
+    quote::quote! {
+        impl protocol_core::rw::SyncWriteable for #ident {
+            fn write_sync<T>(&self, mut write: &mut T) -> anyhow::Result<()>
+            where
+                T: std::io::Write
+            {
+                use protocol_core::rw::SyncWriteable;
+
+                match &self {
+                    #match_body
+                    _ => panic!()
                 }
+
+                Ok(())
+            }
+        }
+    }
+}
+
+fn write_inputs(fields: &syn::Fields, has_self: bool) -> proc_macro2::TokenStream {
+    let mut result_token_stream = proc_macro2::TokenStream::new();
+    for field in fields {
+        match &field.ty {
+            syn::Type::Path(_) => {
+                let ident = &field.ident;
+                let accessor = if has_self {
+                    quote::quote! { self. }
+                } else {
+                    quote::quote!()
+                };
+
+                result_token_stream.extend_one(quote! {
+                    #accessor #ident.write_sync(write)?;
+                });
+            }
+            _ => {
+                panic!()
             }
         };
-        return tokens.into();
     }
-    proc_macro2::TokenStream::new()
+
+    result_token_stream
 }
